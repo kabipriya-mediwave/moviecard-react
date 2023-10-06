1faf83ceb1415343cdb6c5363b9190394f9a1a7e
kabipriya-mediwave <kabipriya@mindwaveventures.com>
2023-10-06 13:45:07 +0530

    final

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Formik, Form, Field } from 'formik';
-import Button from './Components/button';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import Button from './Components/button'; 
 import * as Yup from 'yup';
 import './App.css';
+import Card from './Components/card';
 function App() {
   const initialValues = {
     title: '',
@@ -10,26 +11,24 @@ function App() {
     ratings: '',
     url: '',
   };
+
   const validationSchema = Yup.object().shape({
     title: Yup.string().required('Title required'),
     description: Yup.string().required('Description required').min(5,"Description should be at least 5 characters"),
     ratings: Yup.number().required('Ratings is required').max(10,"Rating cannot be greater than 10"),
     url: Yup.string().url('Enter a valid URL').required('URL is required'),
   });
-  const [CardInfo, setCardInfo] = useState([]);
+
+  const [cardData, setCardData] = useState([]);
+
   const handleSubmit = (values, { resetForm }) => {
     console.log(values);
-    const cardContent = (
-      <div className="card">
-        <h3>{values.title}</h3>
-        <p>Description: {values.description}</p>
-        <p>Ratings: {values.ratings}</p>
-        <img src={values.url} alt="Image" />
-      </div>
-    );
-    setCardInfo([...CardInfo,cardContent]);
+
+    setCardData([...cardData,values]);
+    
     resetForm();
   };
+
   return (
     <div>
       <Formik
@@ -90,11 +89,24 @@ function App() {
             <Button type="submit" name="Submit" />
           </Form>
         )}
-      </Formik>
-      <div className="total-card">
-      {CardInfo}
+      </Formik> 
+      <div>
+        
+          {/* cardData.map{(item)=>{
+            return <card/>
+          }
+
+          } */}
+          <div className="card">
+        {cardData &&
+          cardData.map((item) => {
+            return <Card values={item} />;
+          })}
+      </div>
+      
       </div>
     </div>
   );
 }
+
 export default App;
